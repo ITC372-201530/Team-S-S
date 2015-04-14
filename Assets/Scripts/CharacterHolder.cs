77eeb0e658a0b52a3efef1168e595b2eca6d7e05
patphyo <patphyo@gmail.com>
2015-04-14 21:23:41 +1000

    Added files that are not versioned.
    Signed-off-by: patphyo <patphyo@gmail.com>

---
+++ Assets/Scripts/CharacterHolder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterHolder : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.transform.tag.Equals ("Player") || col.transform.tag.Equals ("Pushable"))
+		col.transform.parent = gameObject.transform;
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		col.transform.parent = null;
+	}
+
+}
---
+++ Assets/Scripts/HideShadow.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideShadow : MonoBehaviour {
+	GameObject [] shadows;
+	public GameObject player;
+	public GameObject pushable;
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.tag == "Sun") 
+		{
+		
+			if (player.transform.parent != null)
+			{
+				if (player.transform.parent.tag.Equals ("ShadowHolder"))
+				{
+					player.transform.parent = null;
+				}
+			}
+
+			if (pushable.transform.parent != null)
+			{
+				if (pushable.transform.parent.tag.Equals ("ShadowHolder"))
+				{
+					pushable.transform.parent = null;
+				}
+			}
+
+			shadows = GameObject.FindGameObjectsWithTag ("Shadow");
+			foreach (GameObject sdw in shadows) 
+			{
+				sdw.SetActive (false);
+			}
+		
+		}
+	}
+	
+	void OnTriggerExit (Collider col)
+	{
+		if (col.tag == "Sun") 
+		{
+			foreach (GameObject sdw in shadows) 
+			{
+				sdw.SetActive (true);
+				sdw.collider.enabled = false;
+			}
+		}
+	}
+
+}
